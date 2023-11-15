2ae634ca039e9bb975977a49c5a05382fe9eb2d3
nadering <120453768+nadering@users.noreply.github.com>
2023-11-16 02:09:10 +0900

    Design: Add pre-line
    Add "white-space: pre-line" to question content, example answer to make a newline

--- src/app/[topic]/_question/example-answer.tsx
+++ src/app/[topic]/_question/example-answer.tsx
@@ -5,7 +5,7 @@ export default function ExampleAnswer({ id, content }: { id: number, content: st
   return (
     <div className="vbox gap(4) py(4)">
       <p className="paragraph text-small c(--content-secondary)">모범 답안</p>
-      <p className="paragraph text-medium c(--primary) line-height(1.5)">{content}</p>
+      <p className="paragraph text-medium c(--primary) line-height(1.5) pre-line">{content}</p>
     </div>
   );
 }
--- src/app/[topic]/_question/question-content.tsx
+++ src/app/[topic]/_question/question-content.tsx
@@ -12,7 +12,7 @@ export default function QuestionContent({ question, onClick }: QuestionContentPr
       <p className="paragraph text-small text-center w(30~) c(--content-secondary)">
         {String(question.id).padStart(3, "0")}
       </p>
-      <p className="paragraph text-medium w(~100%-50px) c(--primary) break-word">{question.content}</p>
+      <p className="paragraph text-medium w(~100%-50px) c(--primary) break-word pre-line">{question.content}</p>
     </div>
   );
 }
